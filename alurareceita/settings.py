cc475fe303b9d9268f7f45533998749741a182a0
lucas1606 <lucas.almeida1606@gmail.com>
2021-08-26 11:44:59 -0300

    Criada pasta, e campo para postagem de imagem no paniel de administrador

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -129,3 +129,6 @@ STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 STATICFILER_DIRS = [
     os.path.join(BASE_DIR, 'receitas/static')
 ]
+# Media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- receitas/admin.py
+++ receitas/admin.py
@@ -6,11 +6,12 @@ from .models import Receita
 #serão essas:
 #       'id', 'nome_receita', 'categoria', 'tempo_preparo'
 class ListandoReceitas(admin.ModelAdmin):
-    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo') #informações de cada linha do banco
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo', 'publicada') #informações de cada linha do banco
     list_display_links = ('id', 'nome_receita')# colunas clickaveis para abrir linha no cdud
     search_fields = ('nome_receita',) # adicionando campo de busca por nome da receita a "," serve para que seja um tupla
     list_filter = ('categoria',) # adicionando filto por categoria "," novamente tupla
-    list_per_page = 2 #paginação do django admin
+    list_per_page = 5 #paginação do django admin
+    list_editable = ('publicada',)# definindo o que pode ser editável diretamente na lista de edição, sem abrir a receitad
 admin.site.register(Receita, ListandoReceitas)
 
 # Register your models here.
